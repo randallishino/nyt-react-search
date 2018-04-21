cc61cda742fcc03cca11ad06403af21ca41254c0
randall ishino <rishino24@gmail.com>
2018-04-21 02:02:45 -0700

    fix jumbotron, add responsive nav

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Jumbotron = () =>
   <div className="jumbotron text-center">
-    <h2>(ReactJS) New York Times React</h2>
+    <h2> New York Times with React</h2>
     <h3>Search and save articles that you like!.</h3>
   </div>;
 
