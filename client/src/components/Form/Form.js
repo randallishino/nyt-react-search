d06584e3291d8bbad1359108c8ad5db770f4daca
randall ishino <rishino24@gmail.com>
2018-04-21 01:59:02 -0700

    add jumbotron component/form component

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+const Input = props =>
+  <div className="form-group">
+    <input className="form-control" {...props} />
+  </div>;
+
+
+export default Input;
---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Jumbotron = () =>
+  <div className="jumbotron text-center">
+    <h2>(ReactJS) New York Times React</h2>
+    <h3>Search and save articles that you like!.</h3>
+  </div>;
+
+export default Jumbotron;
